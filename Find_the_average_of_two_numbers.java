7d48994fe3e9a22dbf3f02f95a33e4e68d78f28b
Mohana Siri Vasamsetty <21A95A0506@aec.edu.in>
2022-10-13 14:12:33 +0530

    import java.util.*; class Demo {     public static void main(String args[])     {         float a,b,c;         Scanner sc=new Scanner(System.in);         a=sc.nextFloat();         b=sc.nextFloat();         c=(a+b)/2;         System.out.printf("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String args[])
+    {
+        float a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        c=(a+b)/2;
+        System.out.printf("%.4f",c);
+    }
+}
