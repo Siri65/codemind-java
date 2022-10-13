663ddc9769170a0ad4c2e96eda0ce252a2fde9c6
Mohana Siri Vasamsetty <21A95A0506@aec.edu.in>
2022-10-13 21:55:26 +0530

    import java.util.*; class Demo {     public static void main(String args[])     {         int a,b,res;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         res=a%b;         System.out.print(res);     } }

---
+++ Modulus.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String args[])
+    {
+        int a,b,res;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        res=a%b;
+        System.out.print(res);
+    }
+}
