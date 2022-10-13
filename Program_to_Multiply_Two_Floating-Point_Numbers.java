0731148fc58601c2b584fd96e1ead98051bbc630
Mohana Siri Vasamsetty <21A95A0506@aec.edu.in>
2022-10-13 20:39:10 +0530

    import java.util.*; class Demo {     public static void main(String args[])     {         float a,b,res;         Scanner sc=new Scanner(System.in);         a=sc.nextFloat();         b=sc.nextFloat();         res=a*b;         System.out.printf("%.2f",res);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String args[])
+    {
+        float a,b,res;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        res=a*b;
+        System.out.printf("%.2f",res);
+    }
+}
