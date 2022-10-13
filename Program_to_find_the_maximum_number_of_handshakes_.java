9ace55e9ace050f71a20ed2c3604d9e381a62923
Mohana Siri Vasamsetty <21A95A0506@aec.edu.in>
2022-10-13 22:08:23 +0530

    import java.util.*; class Demo {     public static void main(String args[])     {         int x,res;         Scanner sc=new Scanner(System.in);         x=sc.nextInt();         res=(x*(x-1))/2;         System.out.print(res);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String args[])
+    {
+        int x,res;
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextInt();
+        res=(x*(x-1))/2;
+        System.out.print(res);
+    }
+}
