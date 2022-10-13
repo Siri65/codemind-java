db02796029d17536db00130dcacf72da30a1da90
Mohana Siri Vasamsetty <21A95A0506@aec.edu.in>
2022-10-13 13:34:14 +0530

    import java.util.*; class Demo {     public static void main(String args[])     {         float num,area;         Scanner sc=new Scanner(System.in);         num=sc.nextFloat();         area=3.14f*num*num;         System.out.print(area);     }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String args[])
+    {
+        float num,area;
+        Scanner sc=new Scanner(System.in);
+        num=sc.nextFloat();
+        area=3.14f*num*num;
+        System.out.print(area);
+    }
+    
+}
