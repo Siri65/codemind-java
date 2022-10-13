be7480493a98f784111cf65bb04a9090ad0a40b8
Mohana Siri Vasamsetty <21A95A0506@aec.edu.in>
2022-10-13 19:23:55 +0530

    import java.util.*; class Demo {     public static void main(String args[])     {         char str;         Scanner sc=new Scanner(System.in);         str=sc.next().charAt(0);         int res = str;         System.out.print(res);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String args[])
+    {
+        char str;
+        Scanner sc=new Scanner(System.in);
+        str=sc.next().charAt(0);  
+        int res = str;
+        System.out.print(res);
+    }
+}
