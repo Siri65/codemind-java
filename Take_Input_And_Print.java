509c4f37dd7c488701d677ef721d7d54ee23c6d5
Mohana Siri Vasamsetty <21A95A0506@aec.edu.in>
2022-10-13 22:00:01 +0530

    import java.util.*; class Demo {     public static void main(String args[])     {         String str;         Scanner sc=new Scanner(System.in);         str=sc.nextLine();         System.out.print(str);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String args[])
+    {
+        String str;
+        Scanner sc=new Scanner(System.in);
+        str=sc.nextLine();
+        System.out.print(str);
+    }
+}
